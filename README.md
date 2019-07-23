89ceb2854486d95e581fee9e3611c6bb08e3c9b8
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2019-07-23 21:40:54 +0300

    #7 As User, I want use UI to find english sentence

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # english learning
+
+# install
+1. install nodejs msi
+2. install yarn msi
+
+
+# other
+
+npm set progress=false
+npm i --prefer-offline
+
+npm install npm-proxy-cache -g
+npm-proxy-cache --expired --ttl=86400 --host=0.0.0.0
+
+npx
+yarn
+pnpm
+npm-fast-install
+
+babel 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import initStore from 'config/store';
+import { setupAxiosInterceptors } from 'rest/axios';
+import DevTools from 'config/devtools';
+import { redirectToLoginWithMessage, logout } from 'reducers/authentication';
+import { browserHistory } from 'react-router'
+import { syncHistoryWithStore } from 'react-router-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Router } from 'react-router';
+import getRoutes from 'router/router';
+import { registerLocales } from 'config/translation';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const devTools = process.env.NODE_ENV === 'development' ? <DevTools /> : null;
+
+const store = initStore();
+const history = syncHistoryWithStore(browserHistory, store);
+registerLocales(store);
+
+const actions = bindActionCreators({redirectToLoginWithMessage, logout}, store.dispatch);
+setupAxiosInterceptors(() => actions.redirectToLoginWithMessage('login.error.unauthorized'));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <div>
+      {devTools}
+      <Router history={history} routes={getRoutes(actions.logout)}/>
+    </div>
+  </Provider>,
+  document.getElementById('root')
+);

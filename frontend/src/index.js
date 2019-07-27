f3a0f395a5be075410e8685f12ea61fef0d92db3
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2019-07-27 21:38:25 +0300

    #7 As User, I want use UI to find engish sentence

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,30 +3,26 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import initStore from 'config/store';
-import { setupAxiosInterceptors } from 'rest/axios';
 import DevTools from 'config/devtools';
-import { redirectToLoginWithMessage, logout } from 'reducers/authentication';
 import { hashHistory } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
 
 import { Router } from 'react-router';
 import getRoutes from 'router/router';
-import { registerLocales } from 'config/translation';
 
-const devTools = process.env.NODE_ENV === 'development' ? <DevTools /> : null;
+const devTools = null;
+// const devTools = process.env.NODE_ENV === 'development' ? <DevTools /> : null;
 
 const store = initStore();
 const history = syncHistoryWithStore(hashHistory, store);
-registerLocales(store);
 
-const actions = bindActionCreators({redirectToLoginWithMessage, logout}, store.dispatch);
-setupAxiosInterceptors(() => actions.redirectToLoginWithMessage('login.error.unauthorized'));
+const actions = bindActionCreators({}, store.dispatch);
 
 ReactDOM.render(
   <Provider store={store}>
     <div>
       {devTools}
-      <Router history={history} routes={getRoutes(actions.logout)}/>
+      <Router history={history} routes={getRoutes()}/>
     </div>
   </Provider>,
   document.getElementById('root')
--- frontend/src/ui/container/SimpleListComponent.js
+++ frontend/src/ui/container/SimpleListComponent.js
@@ -1,6 +1,25 @@
-import ListComponent from 'component/ListComponent';
+import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {fetchSimple} from 'reducers/simple';
+
+export class ListComponent extends Component {
+    render() {
+        const items = this.props.items;
+        const list = items.map((item, index) => <li key={index}>{item}</li>);
+
+        return (
+            <div>
+                <h2>Search result:</h2>
+                <ul>
+                    {list}
+                </ul>
+                <button onClick={this.props.fetchSimple}>
+                    Fetch
+                </button>
+            </div>
+        );
+    }
+}
 
 export default connect(
   state => ({items: state.simple.items}),

f8496fe8a1ad733b7ed5c7b90b6f64dc17b4de06
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2019-07-30 01:00:27 +0300

    As User, I want search screen willbe on all width #12 As Admin, I want admin screen will be on all width #13

--- frontend/src/ui/container/AdminPage.js
+++ frontend/src/ui/container/AdminPage.js
@@ -5,8 +5,8 @@ import {indexBook} from './../../actions/parse'
 export class AdminPage extends Component {
     render() {
         return (
-            <div className="row mt-2">
-                <div className="col-12">Rebuild index form all book locate in source directory
+            <div className="container mt-2 card">
+                <div className="col-12 card-body">Rebuild index form all book locate in source directory
                     <input type="button" value="Build Index" className="btn btn-outline-secondary float-right"
                            onClick={this.props.indexBook}/>
                 </div>

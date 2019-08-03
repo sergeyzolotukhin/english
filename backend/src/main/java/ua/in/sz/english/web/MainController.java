916a58948c1660eebb48c96538e5c943795e9c76
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2019-08-03 18:50:14 +0300

    As User I want identify parts of speech (noun, verb) #14. Disable because it is slow work

--- backend/src/main/java/ua/in/sz/english/web/MainController.java
+++ backend/src/main/java/ua/in/sz/english/web/MainController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ua.in.sz.english.service.AdminService;
-import ua.in.sz.english.service.PartOfSpeechService;
 import ua.in.sz.english.service.SearchService;
 
 import java.util.List;
@@ -16,22 +15,18 @@ import java.util.List;
 @RequestMapping("/api")
 public class MainController {
     private final SearchService searchService;
-    private final PartOfSpeechService partOfSpeechService;
     private final AdminService adminService;
 
     @Autowired
-    public MainController(SearchService searchService, PartOfSpeechService partOfSpeechService,
-                          AdminService adminService) {
+    public MainController(SearchService searchService, AdminService adminService) {
         this.searchService = searchService;
-        this.partOfSpeechService = partOfSpeechService;
         this.adminService = adminService;
     }
 
     @RequestMapping("/search")
     public List<String> search(@RequestParam("query") String query,
                                @RequestParam(value = "limit", required = false, defaultValue = "20") int limit) {
-        return partOfSpeechService.tagging(
-                searchService.search(query, limit));
+        return searchService.search(query, limit);
     }
 
     @RequestMapping("/index/book")

99f02ea4fe42b3bcf0fa24c12f45be493f2d7b73
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2019-10-21 22:17:22 +0300

    As Dev I want migrate to spring batch for take extendability. #18

--- backend/src/test/java/ua/in/sz/english/batch/parser/book/DescriptionTest.java
+++ backend/src/test/java/ua/in/sz/english/batch/parser/book/DescriptionTest.java
@@ -3,24 +3,23 @@ package ua.in.sz.english.batch.parser.book;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @Slf4j
 class DescriptionTest {
 
-	private static final int WORD = 1;
-	private static final int TRANSCRIPTION = 2;
-	private static final int PART_OF_SPEECH = 3;
-	private static final int DEFINITION = 4;
-
 	private static final String WORD_PATTERN = "^(\\S+\\s*)";
 	private static final String TRANSCRIPTION_PATTERN = "(\\[\\S*]\\s*)";
 	private static final String PART_OF_SPEECH_PATTERN = "(\\S+\\s+)";
-	private static final String DEFINITION_PATTERN = "(.*)";
+	private static final String DESCRIPTION_LIST_PATTERN = "(.*)";
 
-	private static Pattern DESCRIPTION_PATTERN = Pattern.compile(
-			WORD_PATTERN + TRANSCRIPTION_PATTERN + PART_OF_SPEECH_PATTERN + DEFINITION_PATTERN);
+	private static Pattern DEFINITION_PATTERN = Pattern.compile(
+			WORD_PATTERN + TRANSCRIPTION_PATTERN + PART_OF_SPEECH_PATTERN + DESCRIPTION_LIST_PATTERN);
+
+	private static Pattern DESCRIPTION_PATTERN = Pattern.compile("(\\d+[^\\d]+)\\s*");
 
 	@Test
 	void parse() {
@@ -33,17 +32,31 @@ class DescriptionTest {
 				"6) юр.  аннулировать, отменять, прекращать; " +
 				"7) метал, отпускать (сталь).";
 
-		Matcher matcher = DESCRIPTION_PATTERN.matcher(description);
+		Matcher matcher = DEFINITION_PATTERN.matcher(description);
 		if (matcher.find()) {
-			log.info("group count:[{}]", matcher.groupCount());
+			String word = matcher.group(1);
+			String transcription = matcher.group(2);
+			String partOfSpeech = matcher.group(3);
+			String descriptions = matcher.group(4);
 
-			log.info("word:[{}]", matcher.group(WORD));
-			log.info("transcription:[{}]", matcher.group(TRANSCRIPTION));
-			log.info("part:[{}]", matcher.group(PART_OF_SPEECH));
+			log.info("word:[{}]", word);
+			log.info("transcription:[{}]", transcription);
+			log.info("part:[{}]", partOfSpeech);
 
-			log.info("definition:[{}]", matcher.group(DEFINITION));
+			parseDescriptions(descriptions).forEach(s -> log.info("description: [{}]", s));
 		} else {
 			log.error("Not working");
 		}
+	}
+
+	private List<String> parseDescriptions(String descriptions) {
+		List<String> result = new ArrayList<>();
+
+		Matcher matcher = DESCRIPTION_PATTERN.matcher(descriptions);
+		while (matcher.find()) {
+			result.add(matcher.group(1));
+		}
+
+		return result;
 	}
 }

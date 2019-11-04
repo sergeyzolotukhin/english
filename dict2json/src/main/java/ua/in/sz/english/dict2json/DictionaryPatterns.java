7c76a6f1b37afbacbbe2938ddfaf1a8f518bd914
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2019-11-04 23:08:16 +0200

    As Dev I want migrate to spring batch for take extendability. #18

--- dict2json/src/main/java/ua/in/sz/english/dict2json/DictionaryPatterns.java
+++ dict2json/src/main/java/ua/in/sz/english/dict2json/DictionaryPatterns.java
@@ -2,14 +2,16 @@ package ua.in.sz.english.dict2json;
 
 
 public final class DictionaryPatterns {
+	public static final String START = "^";
+	public static final String END = "\\.";
+
+	public static final String MEANING = "(([а-яёА-ЯЁ'\\-]+\\s+)*([а-яёА-ЯЁ'\\-]+))";
+
 	public static final String WORD = "^\\s*([a-zA-Z\\-]+)\\s*";
 	public static final String ABBREVIATION = "\\s*([a-zA-Zа-яёА-ЯЁ]+)\\.\\s*";
 	public static final String DEFINITION_NO = "\\s*\\d+\\.\\s+";
 	public static final String PART_OF_SPEECH = "\\s*([а-яёА-ЯЁa-zA-Z]+)\\s+";
-	public static final String MEANING = "(([а-яёА-ЯЁ'\\-]+\\s+)*([а-яёА-ЯЁ'\\-]+))";
 	public static final String TRANSCRIPTION = "\\s*\\[([a-zA-Z':, \\^]+)]\\s*";
-	public static final String START = "^";
-	public static final String END = "\\.";
 
 	public static final String RU_WORD = "^\\s*([а-яёА-ЯЁ\\-]+)\\s*";
 	public static final String RU_SUFFIX = "\\s*(\\(\\s*[а-яёА-ЯЁ]+\\s*\\))\\s*";

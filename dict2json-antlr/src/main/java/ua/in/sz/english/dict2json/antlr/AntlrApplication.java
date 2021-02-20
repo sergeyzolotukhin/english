c9de49592c06ffe0ebf8020c5c8d55e31eb2dda0
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2021-02-20 17:21:39 +0200

    Gradle wrapper

--- dict2json-antlr/src/main/java/ua/in/sz/english/dict2json/antlr/AntlrApplication.java
+++ dict2json-antlr/src/main/java/ua/in/sz/english/dict2json/antlr/AntlrApplication.java
@@ -4,15 +4,22 @@ import lombok.extern.slf4j.Slf4j;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 
+import java.nio.charset.Charset;
 import java.util.List;
 
+// -Dfile.encoding=UTF-16
 @Slf4j
 public class AntlrApplication {
 	private static final String TEXT = "" +
 			"aard-wolf ['a:d,wulfj] n земляной волк.\n" +
-			"abashment [s'beejrmnt] n смуще'ние, замешательство.\n";
+			"abashment [s'beejrmnt] n смушение смуще'ние, замешательство.\n";
 
 	public static void main(String[] args) {
+		Charset charset = Charset.defaultCharset();
+		log.info("The charset is [{}]", charset);
+		System.out.println("Сергей =>" + TEXT);
+		log.info("The text is [{}]", TEXT);
+
 		DictionaryLexer lexer = new DictionaryLexer(CharStreams.fromString(TEXT));
 		DictionaryParser parser = new DictionaryParser(new CommonTokenStream(lexer));
 

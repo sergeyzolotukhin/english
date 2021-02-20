f765602c95594060996ef2b2fa4c88dda665d143
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2021-02-20 18:14:23 +0200

    Gradle wrapper

--- dict2json-antlr/src/main/java/ua/in/sz/english/dict2json/antlr/AntlrApplication.java
+++ dict2json-antlr/src/main/java/ua/in/sz/english/dict2json/antlr/AntlrApplication.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 
+import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
 import java.util.List;
 
@@ -14,11 +15,13 @@ public class AntlrApplication {
 			"aard-wolf ['a:d,wulfj] n земляной волк.\n" +
 			"abashment [s'beejrmnt] n смушение смуще'ние, замешательство.\n";
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		Charset charset = Charset.defaultCharset();
 		log.info("The charset is [{}]", charset);
 		System.out.println("Сергей =>" + TEXT);
 		log.info("The text is [{}]", TEXT);
+
+//		String result = new String(TEXT.getBytes("windows-1251"), Charset.forName("UTF-8"));
 
 		DictionaryLexer lexer = new DictionaryLexer(CharStreams.fromString(TEXT));
 		DictionaryParser parser = new DictionaryParser(new CommonTokenStream(lexer));
